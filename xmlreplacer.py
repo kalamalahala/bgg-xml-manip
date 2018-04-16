45ef687611d103bd5137d3312dffb0a5d093f1ee
kalamalahala <36871247+kalamalahala@users.noreply.github.com>
2018-04-16 10:43:15 -0400

    old stuff

--- xmlreplacer.py
+++
@@ -1,9 +0,0 @@
-import xml.etree.ElementTree as etree
-
-tree = etree.parse('collection-extended-workable.xml')
-root = tree.getroot()
-print (root)
-  
-names = root[0][0].findall('name')
-print (names.content)
-print (root[0][0][0])
